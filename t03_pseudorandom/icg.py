ac66b86f186c7a3184115a663a298119221927e8
lucas <lucasjoao.lj@gmail.com>
2017-08-31 22:55:21 -0300

    start t03

---
+++ t03_pseudorandom/icg.py
@@ -0,0 +1,11 @@
+class icg:
+    def __init__(self, seed, q, a, c):
+        self.seed = seed
+        self.q = q
+        self.a = a
+        self.c = c
+
+    def generate(self, n):
+        result = [seed]
+        for i in range(n):
+            if
